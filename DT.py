8d94e1312f86bd43e228f93356ff28b3aa3932d7
minxingzhang0107 <minxing.zhang@emory.edu>
2021-11-29 11:49:44 -0500

    lalala

--- DT.py
+++ DT.py
@@ -1,10 +1,11 @@
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.datasets import make_classification
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 # load x_train, y_train, x_test, y_test
@@ -22,13 +23,26 @@ x_train = np.array(x_train)
 x_test = np.array(x_test)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
-# apply the random forest regressor
-rf = RandomForestRegressor(n_estimators=100, max_depth=255, random_state=0)
-rf.fit(x_train, y_train.flatten())
-y_pred = rf.predict(x_test)
+# apply the decision tree regression
+clf = DecisionTreeRegressor(max_depth=255)
+clf.fit(x_train, y_train.flatten())
+y_pred = clf.predict(x_test)
 # calculate the root mean squared error
 rmse = np.sqrt(np.mean((y_pred - y_test) ** 2))
-print('RMSE: ', rmse)
+print('RMSE:', rmse)
+# use grid search to find the best parameters
+parameters = {"criterion": ["squared_error", "friedman_mse", "absolute_error", "poisson"],
+            "max_depth": range(1, 15),
+            "min_samples_leaf": range(1, 100, 10),
+            "splitter": ["best", "random"],
+            "min_weight_fraction_leaf": [0.1, 0.3, 0.5, 0.7, 0.9],
+            "max_features": ["auto", "log2", "sqrt", None],
+            "max_leaf_nodes": [None, 10, 30, 50, 70, 90]
+            }
+model_tuning = GridSearchCV(DecisionTreeRegressor(), param_grid=parameters, scoring='neg_root_mean_squared_error'
+                          ,cv=5)
+model_tuning.fit(x_train, y_train.flatten())
+print(model_tuning.best_params_)
 
 
 # load x_train, y_train, x_test, y_test
@@ -44,13 +58,11 @@ x_train_pca = np.array(x_train_pca)
 x_test_pca = np.array(x_test_pca)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
-# apply the random forest regressor
-rf = RandomForestRegressor(n_estimators=100, max_depth=255, random_state=0)
-rf.fit(x_train_pca, y_train.flatten())
-y_pred = rf.predict(x_test_pca)
+# apply the decision tree regression
+clf = DecisionTreeRegressor(max_depth=255)
+clf.fit(x_train_pca, y_train.flatten())
+y_pred = clf.predict(x_test_pca)
 # calculate the root mean squared error
 rmse = np.sqrt(np.mean((y_pred - y_test) ** 2))
-print('RMSE (pca): ', rmse)
-
-
+print('RMSE (pca):', rmse)
 
